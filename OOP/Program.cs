cbe3b6c7d9aa1e64f99046f03c2b329d29ae7556
GeraltOfRhivia <sp22575@gmail.com>
2018-07-18 23:23:14 +0300

    Добавьте файлы проекта.

---
+++ OOP/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+
+    class Test
+    {
+        // FIELDS
+        private string name;
+        private int age;
+        
+        // CONSTRUCT
+        public Test()
+        {
+            Console.WriteLine("Create Test-class");
+        }
+
+        // SET & GET
+        // CСоздается нажав правую кнопку мыши на полях
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+
+
+    }
+
+}
